ac9175b1d24dd6ff12ad03ae0a80c32ad2e60cfa
gmlwns0704 <gmlwns0704@gmail.com>
2023-05-03 06:37:48 +0000

    auto

--- getProc.c
+++ getProc.c
@@ -52,7 +52,7 @@ struct procInfo* getProcInfoByPort(struct procInfo* info, int port){
         return NULL;
     }
 
-    // printf(buff);
+    printf(buff);
     char localAddrBuff[32];
     char foreignAddrBuff[32];
     // Proto Recv-Q Send-Q Local_Address Foreign_Address State PID/Program name
@@ -71,4 +71,64 @@ struct procInfo* getProcInfoByPort(struct procInfo* info, int port){
     pclose(f);
 
     return info;
+}
+
+int getMulProcInfoByPort(struct procInfo* infoArr, size_t num, int port){
+    // 명령어 출력 결과 버퍼
+    char buff[BUFSIZ];
+    memset(buff, 0, sizeof(buff));
+
+    // 명령어 설정
+    char command[BUFSIZ];
+    sprintf(command, "netstat -ntlp | grep -E [0-9]\\+.[0-9]\\+.[0-9]\\+.[0-9]*:%d\\+' '", port);
+    
+    //결과 버퍼
+    memset(info, 0, sizeof(struct procInfo));
+    
+    FILE* f = popen(command, "r");
+    if (f == NULL){
+        perror("popen");
+        pclose(f);
+        return NULL;
+    }
+    fread(buff, sizeof(buff), 1, f);
+    if (strlen(buff) < 0){
+        perror("fread");
+        pclose(f);
+        return NULL;
+    }
+    else if(strlen(buff) == 0){
+        fprintf(stderr, "process not found\n");
+        pclose(f);
+        return NULL;
+    }
+
+    printf(buff);
+    int offset = 0;
+    int count = 0;
+    for(count = 0; count < num; count++){
+        char line[BUFSIZ];
+        struct connInfo* info;
+
+        sscanf(buff + offset, "%[^\n]\n", line);
+        offset += strlen(line)+1;
+        info = (struct connInfo*)infoArr + count;
+
+        char localAddrBuff[32];
+        char foreignAddrBuff[32];
+        // Proto Recv-Q Send-Q Local_Address Foreign_Address State PID/Program name
+        sscanf(line, "%s %*d %*d %[^: ]:%d %[^: ]:%*s %s %d/%[^: ]\n",
+            info->protocol,
+            localAddrBuff,
+            &(info->port),
+            foreignAddrBuff,
+            info->state,
+            &(info->pid),
+            info->procName);
+        // 주소를 네트워크 바이트로 변환
+        inet_aton(localAddrBuff, &(info->localAddress));
+        inet_aton(foreignAddrBuff, &(info->foreignAddress));
+    }
+
+    return count;
 }
