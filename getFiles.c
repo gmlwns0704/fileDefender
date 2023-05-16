f6b52d4ba3042d7c8aae90d52a2493944e203cbf
HarryBae1011 <111692399+HarryBae1011@users.noreply.github.com>
2023-05-17 01:07:24 +0900

    Update getFiles.c
    findfile 함수만 쓰면 됨
    main은 테스트할 때 넣어놓은거

--- getFiles.c
+++ getFiles.c
@@ -2,24 +2,21 @@
 #include <stdlib.h>
 #include "header/getProc.h"
 #include <string.h>
+#include <stdbool.h>
 
-int main()
-{
-     FILE* fp;
+bool findfile(char* path){
+    FILE* fp;
     struct procInfo procIn;
-    int PID, i=0;
+    int PID;
     //파일 출력하는 버퍼
     char buff[BUFSIZ];
     //sprintf 임시 버퍼
     char buffer[BUFSIZ];
-    //파일 목록 저장 포인터 배열
-    char *files[BUFSIZ];
 
     //포트 번호로 프로세스 아이디 받아오기
     getProcInfoByPort(&procIn, 22);
 
     PID = procIn.pid;
-
     sprintf(buffer, "lsof +p %d | tr -s ' ' | cut -d' ' -f9 ", PID);
 
     fp = popen(buffer, "r");
@@ -28,14 +25,32 @@ int main()
         perror("erro : ");
     }
 
-    while (fgets(buff, 200, fp) != NULL)
+    while (fgets(buff, 100, fp) != NULL)
     {
-        //파일이름 하나씩 포인터 배열에 저장
-        files[i] = buff;
-        printf("%s", files[i]);
-        i++;
+        if(strncmp(path, buff, strlen(path)) == 0)
+        {
+            printf("검색된 파일 이름 : %s\n", buff);
+            return true;
+        }
     }
 
     pclose(fp);
 
+    return false;
+}
+
+
+int main()
+{
+    char* filename;
+    //file에 검색할 파일 경로 넣으면 됨
+    char file[30] = "/home";
+    bool flag;
+
+    filename = file;
+    flag = findfile(filename);
+    if(flag == true)
+        printf("파일 검색 완료\n");
+    else
+        printf("그런 파일은 없습니다.\n");
 }
