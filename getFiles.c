5528a4dac90abf0beaf5a985895f2b7421b6e375
HarryBae1011 <111692399+HarryBae1011@users.noreply.github.com>
2023-05-23 23:31:56 +0900

    grep으로 검색

--- getFiles.c
+++ getFiles.c
@@ -24,7 +24,7 @@ int findfile(int pid, int filecount, char*** filelist_buffer, char* path[]){
     {
         for(int i = 0 ; i < filecount ; i++)
         {
-            sprintf(buffer, "lsof +p %d | tr -s ' ' | cut -d' ' -f9 ", pids[h]);
+            sprintf(buffer, "lsof +p %d | tr -s ' ' | cut -d' ' -f9 | grep \"%s\"", pids[h], path[i]);
 
             fp = popen(buffer, "r");
             if (fp == NULL)
@@ -36,13 +36,11 @@ int findfile(int pid, int filecount, char*** filelist_buffer, char* path[]){
             {
                 if(strlen(path[i]) != 0)
                 {
-                    if(strncmp(path[i], buff, strlen(path[i])) == 0)
-                    {
-                        //printf("Pid %d : Path%d -> 검색된 파일 이름 : %s\n", pids[h], i+1, buff);
-                        cnt++;
-                    }
+                    //printf("Pid %d : Path%d -> 검색된 파일 이름 : %s\n", pids[h], i+1, buff);
+                    cnt++;
                 }
             }
+            pclose(fp);
         }
     }
 
@@ -52,11 +50,11 @@ int findfile(int pid, int filecount, char*** filelist_buffer, char* path[]){
         (*filelist_buffer)[j] = (char*)malloc(sizeof(char) * 256); 
     }
 
-    for(int h = 0 ; h < pid_cnt; h++)
+    for(int h = 0 ; h < pid_cnt ; h++)
     {
         for(int i = 0 ; i < filecount ; i++)
         {
-            sprintf(buffer, "lsof +p %d | tr -s ' ' | cut -d' ' -f9 ", pids[h]);
+            sprintf(buffer, "lsof +p %d | tr -s ' ' | cut -d' ' -f9 | grep \"%s\"", pids[h], path[i]);
 
             fp = popen(buffer, "r");
             if (fp == NULL)
@@ -67,15 +65,12 @@ int findfile(int pid, int filecount, char*** filelist_buffer, char* path[]){
             {
                 if(strlen(path[i]) != 0)
                 {
-                    if(strncmp(path[i], buff, strlen(path[i])) == 0)
-                    {
-                        strcpy((*filelist_buffer)[k], buff);
-                        k++;
-                    }
+                    strcpy((*filelist_buffer)[k], buff);  
+                    k++;
                 }
             }
+            pclose(fp);
         }
-        pclose(fp);
     }
 
     free(pids);
@@ -84,37 +79,3 @@ int findfile(int pid, int filecount, char*** filelist_buffer, char* path[]){
     else
         return 0;
 }
-
-/*
-int main()
-{
-    //파일 경로들 저장할 포인터 배열
-    char* filename[100];
-    //검색할 파일들
-    char path1[30] = "/usr/lib";
-    char path2[30] = "/home";
-    char path3[30] = "/";
-    int pid = 1986, flag;
-    //파일목록 저장 버퍼
-    char** buffer;
-
-    int testpid;
-    filename[0] = path1;
-    filename[1] = path2;
-    filename[2] = path3;
-
-    //fork();
-
-    //검색할 파일경로 개수, 파일목록 저장할 버퍼, 파일 경로들 입력
-    flag = findfile(pid, 3, &buffer, filename);
-    if(flag > 0)
-        printf("파일 %d개 검색 완료\n", flag);
-    else
-        printf("그런 파일은 없습니다.\n");
-
-    for(int i = 0 ; i < flag; i++)
-    {
-        printf("%s", buffer[i]);
-    }
-}
-*/
